0565592954bf161dc2d09760afa84ada90c9ea49
Mark Underwood <mark.t.underwood@protonmail.com>
2021-01-30 16:40:20 -0900

    Move evaluation function

--- utilities.py
+++ utilities.py
@@ -77,3 +77,42 @@ def randomMove(board, symbol):
         board, valid = move(board, space[0], space[1], symbol)
 
     return board
+
+
+def evaluate(board):
+    mask = 0b11
+    total = 0
+    winFound = False
+
+    for winState in WIN_STATES:
+        maskedBoard = board & winState
+
+        numberOfXs = 0
+        numberOfOs = 0
+        while maskedBoard != 0:
+            space = (maskedBoard & mask)
+            if space == X_BIN:
+                numberOfXs += 1
+            elif space == O_BIN:
+                numberOfOs += 1
+            maskedBoard = maskedBoard >> 2
+
+        if numberOfXs == 0 and numberOfOs > 0:
+            if numberOfOs == 4:
+                return O_WIN
+
+            winFound = True
+            total -= 1
+        elif numberOfOs == 0 and numberOfXs > 0:
+            if numberOfXs == 4:
+                return X_WIN
+
+            winFound = True
+            total += 1
+
+    if winFound:
+        return total
+    else:
+        return N_WIN
+
+
